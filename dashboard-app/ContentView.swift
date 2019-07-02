c5b265043090453ff2be25db5a23b7100c887f4b
Muneer <NA>
2019-07-02 06:30:35 +0530

    Initial Commit

---
+++ dashboard-app/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  dashboard-app
+//
+//  Created by synup on 02/07/19.
+//  Copyright © 2019 synup. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
